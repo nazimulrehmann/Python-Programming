7d9dbeeae51199cb7a7f94347daf48f672a8cbcb
nazimulrehmann <nazimulrehmann@gmail.com>
2025-07-31 07:54:41 -0700

    uploading basic syntax and fundamentals

---
+++ Variables & Data Types/data_types.py
@@ -0,0 +1,17 @@
+# Variabels and Data Types in Python
+
+# In Python, variables are used to store data values. Python is dynamically typed,
+# meaning you don't need to declare the type of a variable when you create it. 
+# Here are examples of variables with different data types:
+
+# Integer
+age = 25
+print(type(age))  # <class 'int'>
+
+# Float
+price = 19.99
+print(type(price))  # <class 'float'>
+
+# Complex
+complex_num = 3 + 5j
+print(type(complex_num))  # <class 'complex'>
